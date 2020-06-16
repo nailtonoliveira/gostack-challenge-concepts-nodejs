c5752f1d9b18612c6bb5b9c170c50a3f6a834766
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2020-06-16 10:55:40 -0400

    add implementation of update a repository if his exists

--- src/app.js
+++ src/app.js
@@ -31,7 +31,29 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories
+    .findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' });
+  }
+
+  const foundedRepository = repositories[repositoryIndex];
+
+  const repository = {
+    ...foundedRepository,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
